e3088d5931e1c6a3ab05943336934a8e66bf5181
Heathage <jh230155@falmouth.ac.uk>
2020-01-15 12:10:24 +0000

    Implemented a broken crouch. Need help. Will bring up at next meeting.

---
+++ Assets/NextDayDelivery/Scripts/Crouching.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Crouching : MonoBehaviour
+{
+    private CharacterController CharacterController;
+
+    private bool crouch = false;
+
+    private float standing;
+
+    [SerializeField] private float crouching = 0.9f;
+
+    public KeyCode crouchKey = KeyCode.C;
+    void Start()
+    {
+        CharacterController = GetComponent<CharacterController>();
+        standing = CharacterController.height;
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(crouchKey))
+        {
+            crouch = !crouch;
+
+            CheckCrouch();
+        }
+    }
+
+    void CheckCrouch()
+    {
+        if(crouch == true)
+        {
+            CharacterController.height = crouching;
+        }
+        else
+        {
+            CharacterController.height = standing;
+        }
+    }
+}
