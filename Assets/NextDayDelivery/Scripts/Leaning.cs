0a45786e0b4b51ea5c783982b5eae0ed0b1b1f7c
James Heath <jh230155@falmouth.ac.uk>
2020-01-16 16:54:13 +0000

    Created Leaning.cs script for leaning solution.

---
+++ Assets/NextDayDelivery/Scripts/Leaning.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Leaning : MonoBehaviour
+{
+    private CharacterController CharacterController;
+
+    public float leanRight = 0f;
+
+    public float leanLeft = 0f;
+
+    public float noLean = 0f;
+
+    public KeyCode leanRightKey = KeyCode.R;
+    public KeyCode leanLeftKey = KeyCode.Q;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CharacterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(leanRightKey))
+        {
+            CharacterController.height = crouching;
+        }
+        else if (Input.GetKey(leanLeftKey))
+        {
+            CharacterController.height = standing;
+        }
+        else
+        {
+
+        }
+    }
+}
