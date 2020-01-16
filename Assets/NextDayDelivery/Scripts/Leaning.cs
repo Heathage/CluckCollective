b9105d3f7aa2767fae38bea82e71b31148067f1c
James Heath <jh230155@falmouth.ac.uk>
2020-01-16 20:00:46 +0000

    Updated Leaning.cs with a few solutions. None are working.

--- Assets/NextDayDelivery/Scripts/Leaning.cs
+++ Assets/NextDayDelivery/Scripts/Leaning.cs
@@ -2,33 +2,18 @@
 
 public class Leaning : MonoBehaviour
 {
-    private CharacterController CharacterController;
-
-    public float leanRight = 0f;
-
-    public float leanLeft = 0f;
-
-    public float noLean = 0f;
-
     public KeyCode leanRightKey = KeyCode.R;
     public KeyCode leanLeftKey = KeyCode.Q;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        CharacterController = GetComponent<CharacterController>();
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(leanRightKey))
         {
-            CharacterController.height = crouching;
+            transform.rotation = Quaternion.LookRotation(Vector3.forward);
         }
+
         else if (Input.GetKey(leanLeftKey))
         {
-            CharacterController.height = standing;
+            transform.localRotation = Quaternion.Euler(0f, 0f, 90f);
         }
         else
         {
