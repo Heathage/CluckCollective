2a6b4bbaf855483b54116555e589efc8c95c04f8
James Heath <jh230155@falmouth.ac.uk>
2020-01-17 12:58:31 +0000

    Implemented Leaning Prototype!

--- Assets/NextDayDelivery/Scripts/Leaning.cs
+++ Assets/NextDayDelivery/Scripts/Leaning.cs
@@ -2,22 +2,31 @@
 
 public class Leaning : MonoBehaviour
 {
-    public KeyCode leanRightKey = KeyCode.R;
-    public KeyCode leanLeftKey = KeyCode.Q;
+    //public KeyCode leanRightKey = KeyCode.R;
+    //public KeyCode leanLeftKey = KeyCode.Q;
     void Update()
     {
-        if (Input.GetKey(leanRightKey))
+        if (Input.GetKey("r"))
         {
-            transform.rotation = Quaternion.LookRotation(Vector3.forward);
+            Debug.Log("R");
+            
+            transform.localRotation = Quaternion.Euler(0f, 0f, -45f);
+            transform.localPosition = new Vector3(1, 0, 0);
         }
 
-        else if (Input.GetKey(leanLeftKey))
+        else if (Input.GetKeyUp("r"))
         {
-            transform.localRotation = Quaternion.Euler(0f, 0f, 90f);
+            transform.localPosition = new Vector3(0, 0, 0);
         }
-        else
+        else if (Input.GetKey("q"))
         {
-
+            transform.localRotation = Quaternion.Euler(0f, 0f, 45f);
+            transform.localPosition = new Vector3(-1, 0, 0);
         }
+        else if (Input.GetKeyUp("q"))
+        {
+            transform.localPosition = new Vector3(0, 0, 0);
+        }
+        
     }
 }
