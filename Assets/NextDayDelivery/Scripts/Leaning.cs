c7c646598c48b137fc33959d25c0688dc604084a
James Heath <jh230155@falmouth.ac.uk>
2020-01-20 12:54:55 +0000

    Changed values to reflect new height. Changed crouch so that we crouch lower. Changed camera height on Leaning.cs so that the camera is at the right height when leaning.

--- Assets/NextDayDelivery/Scripts/Leaning.cs
+++ Assets/NextDayDelivery/Scripts/Leaning.cs
@@ -10,6 +10,8 @@ public class Leaning : MonoBehaviour
     float leanLeftDistance;
     float leanRightAngle;
 
+    float camHeight = 0.6f;
+
     void Start() 
     {
         leanRightDistance = leanDistance;
@@ -24,21 +26,21 @@ public class Leaning : MonoBehaviour
             Debug.Log("R");
             
             transform.localRotation = Quaternion.Euler(0f, 0f, leanRightAngle);
-            transform.localPosition = new Vector3(leanRightDistance, 1.77f, 0);
+            transform.localPosition = new Vector3(leanRightDistance, camHeight, 0);
         }
 
         else if (Input.GetKeyUp("r"))
         {
-            transform.localPosition = new Vector3(0, 1.77f, 0);
+            transform.localPosition = new Vector3(0, camHeight, 0);
         }
         else if (Input.GetKey("q"))
         {
             transform.localRotation = Quaternion.Euler(0f, 0f, leanLeftAngle);
-            transform.localPosition = new Vector3(leanLeftDistance, 1.77f, 0);
+            transform.localPosition = new Vector3(leanLeftDistance, camHeight, 0);
         }
         else if (Input.GetKeyUp("q"))
         {
-            transform.localPosition = new Vector3(0, 1.77f, 0);
+            transform.localPosition = new Vector3(0, camHeight, 0);
         }
         
     }
