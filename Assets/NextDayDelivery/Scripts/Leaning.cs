498f04a6ee66428d13b0e99dc3f12c3a42fb5e56
Lyra <ts231024@falmouth.ac.uk>
2020-01-20 13:40:48 +0000

    "Improved" the leaning system Leaning is now animated, although vibrations happen

--- Assets/NextDayDelivery/Scripts/Leaning.cs
+++ Assets/NextDayDelivery/Scripts/Leaning.cs
@@ -4,6 +4,7 @@ public class Leaning : MonoBehaviour
 {
     public float leanDistance = 1f;
     public float leanAngle = 30f;
+    public float leanSpeed = 0.1f;
 
     float leanRightDistance;
     float leanLeftAngle;
@@ -24,23 +25,29 @@ public class Leaning : MonoBehaviour
         if (Input.GetKey("r"))
         {
             Debug.Log("R");
-            
-            transform.localRotation = Quaternion.Euler(0f, 0f, leanRightAngle);
-            transform.localPosition = new Vector3(leanRightDistance, camHeight, 0);
+
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, new Quaternion(0f, 0f, leanRightAngle, 0f), leanSpeed * Time.deltaTime);
+            //transform.localRotation = Quaternion.RotateTowards(transform.localRotation, new Quaternion(0f, 0f, leanRightAngle*10,0), leanSpeed);
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, new Vector3(leanRightDistance, camHeight, 0), leanSpeed);
+            //transform.localPosition = new Vector3(leanRightDistance, camHeight, 0);
         }
 
         else if (Input.GetKeyUp("r"))
         {
             transform.localPosition = new Vector3(0, camHeight, 0);
+            //transform.localPosition = Vector3.MoveTowards(new Vector3(leanRightDistance, camHeight, 0), new Vector3(0, camHeight, 0), leanSpeed);
         }
         else if (Input.GetKey("q"))
         {
-            transform.localRotation = Quaternion.Euler(0f, 0f, leanLeftAngle);
-            transform.localPosition = new Vector3(leanLeftDistance, camHeight, 0);
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, new Quaternion(0f, 0f, leanLeftAngle, 0f), leanSpeed * Time.deltaTime);
+            //transform.localRotation = Quaternion.Euler(0f, 0f, leanLeftAngle);
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, new Vector3(leanLeftDistance, camHeight, 0), leanSpeed);
+            //transform.localPosition = new Vector3(leanLeftDistance, camHeight, 0);
         }
         else if (Input.GetKeyUp("q"))
         {
             transform.localPosition = new Vector3(0, camHeight, 0);
+            //transform.localPosition = Vector3.MoveTowards(new Vector3(leanLeftDistance, camHeight, 0), new Vector3(0, camHeight, 0), leanSpeed);
         }
         
     }
