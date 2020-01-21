b8b2bbaa8df61505d251115db03ed963e9c07ad6
TrainerIsaac <isaaclouiswright@gmail.com>
2020-01-21 13:45:41 +0000

    Added Rhythm game, and according assets and scene

---
+++ Assets/NextDayDelivery/RhythmGame/Script/Keypress.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keypress : MonoBehaviour
+{
+    public GameObject hit;
+    public string keyType;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.right* Time.deltaTime);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (Input.GetKey(keyType))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/NextDayDelivery/RhythmGame/Script/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject note;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(note);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
