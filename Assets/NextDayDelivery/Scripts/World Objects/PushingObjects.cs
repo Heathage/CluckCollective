b9a037ad895db9419ee372c96d5aa52da61a5f54
James Heath <jh230155@falmouth.ac.uk>
2020-02-04 11:34:34 +0000

    Added a new cube to testing scene. Added a new script for the pushing of objects. Attached new script to new cube. Started a basic solution on moving the object when mouse button is pushed.

---
+++ Assets/NextDayDelivery/Scripts/World Objects/PushingObjects.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushingObjects : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Debug.Log("Hello");
+    }
+}
