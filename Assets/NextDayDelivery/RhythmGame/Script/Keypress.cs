ec0c7313b9de98953d10448d3f7b9084a7dd30e6
Isaac <isaaclouiswright@gmail.com>
2020-02-04 17:27:56 +0000

    Refreshed and fixed the keypress script, added list of levels that allows the notes spawned to change based on the level

--- Assets/NextDayDelivery/RhythmGame/Script/Keypress.cs
+++ Assets/NextDayDelivery/RhythmGame/Script/Keypress.cs
@@ -1,27 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Keypress : MonoBehaviour
 {
     public GameObject hit;
     public string keyType;
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
+    // Start is called before the first frame update
 
     void Update()
     {
-        transform.Translate(Vector3.right* Time.deltaTime);
+        transform.Translate(-Vector3.right* Time.deltaTime/2); //Moves each note right slowly
+        TextMeshPro textmeshPro = GetComponent<TextMeshPro>(); //references the textmeshpro script attatched to the note
+        textmeshPro.SetText(keyType); //sets the text attatched to textmesh pro as the key type, which is assigned in the spawner script
     }
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerStay2D(Collider2D hit) //If the notes collide with a 2D collider
     {
-        if (Input.GetKey(keyType))
+        if (Input.GetKey(keyType)) //and if the input key is equal to the keytype
         {
-            Destroy(gameObject);
+            Destroy(gameObject); //destroy the note object
         }
-
     }
 }
--- Assets/NextDayDelivery/RhythmGame/Script/Spawner.cs
+++ Assets/NextDayDelivery/RhythmGame/Script/Spawner.cs
@@ -1,19 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Spawner : MonoBehaviour
 {
     public GameObject note;
-    // Start is called before the first frame update
-    void Start()
+    private string[,] lvOne  = new string[,]{ { "z", "v", "n", "d", "f", "i" }, {"s", "h", "r", "e", "k", "4"} }; //Array of strings that are output by the spawner
+    private int est = 1;
+    
+    void Awake()
     {
-        Instantiate(note);
+        StartCoroutine(Test());
     }
 
+
     // Update is called once per frame
-    void Update()
+    IEnumerator Test()
     {
-        
+        for (int i = 0; i < lvOne.Length; i++) // for every item between item equal to 0 and item equal to the length of the lvOne string (in this case 3)
+        {
+            note.GetComponent<Keypress>().keyType = (lvOne[(est),i]); //Sets the keytype of the note to the current item in the array
+            Instantiate(note, gameObject.transform); //instantiates each note
+            yield return new WaitForSeconds(1/3f); //Waits for an amount
+        }
     }
 }
