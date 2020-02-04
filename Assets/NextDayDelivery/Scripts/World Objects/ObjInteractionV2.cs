20f167c5583d602d207ba2f9b83c9ec702de666b
James Heath <jh230155@falmouth.ac.uk>
2020-02-04 17:55:23 +0000

    Added more to script solution. Prevented errors appearing on the script.

--- Assets/NextDayDelivery/Scripts/World Objects/ObjInteractionV2.cs
+++ Assets/NextDayDelivery/Scripts/World Objects/ObjInteractionV2.cs
@@ -13,7 +13,11 @@ public class ObjInteractionV2 : MonoBehaviour
     [SerializeField]
     private bool pickedUp = false;
 
+    Vector3 objectPos;
+
     public LayerMask pickMask;
+
+    RaycastHit pickUpable;
 
     // Update is called once per frame
     void Update()
@@ -21,23 +25,35 @@ public class ObjInteractionV2 : MonoBehaviour
         if (pickedUp == true)
         {
             PickUpObject();
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                pickUpable.collider.GetComponent<Rigidbody>().AddForce(tempParent.transform.forward * throwForce);
+                pickedUp = false;
+            }
+        }
+        else
+        {
+            DropObject();
         }
         Debug.DrawRay(transform.position, transform.forward * 1.5f, Color.blue);
     }
     private void PickUpObject()
     {
-        RaycastHit pickUpable;
-        {
-            if (Physics.Raycast(transform.position, transform.forward, out pickUpable, 1.5f, pickMask))
-            {
-                Debug.Log("Picked Up");
-                pickUpable.collider.GetComponent<Rigidbody>().detectCollisions = false;
-                pickUpable.collider.transform.SetParent(tempParent.transform);
-                pickUpable.collider.GetComponent<Rigidbody>().velocity = Vector3.zero;
-                pickUpable.collider.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            }
-        }
+        Debug.Log("Picked Up");
+        pickUpable.collider.transform.SetParent(tempParent.transform);
+        pickUpable.collider.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        pickUpable.collider.GetComponent<Rigidbody>().velocity = Vector3.zero;
     }
+
+    private void DropObject()
+    {
+        objectPos = pickUpable.collider.transform.position;
+        pickUpable.collider.transform.SetParent(null);
+        pickUpable.collider.GetComponent<Rigidbody>().useGravity = true;
+        pickUpable.transform.position = objectPos;
+    }
+
     private void OnMouseUpAsButton()
     {
         //Checks that the player isn't holding an object and if they are close to the object.
@@ -46,14 +62,13 @@ public class ObjInteractionV2 : MonoBehaviour
             //Disbales gravity so that the object doesn't fall while in the players hands. 
             pickedUp = true;
             pickUpable.collider.GetComponent<Rigidbody>().useGravity = false;
-            item.GetComponent<Rigidbody>().detectCollisions = true;
-
+            pickUpable.collider.GetComponent<Rigidbody>().detectCollisions = false;
         }
 
         //Drops the object.
-        else if (isHolding)
+        else if (pickedUp)
         {
-            isHolding = false;
+            pickedUp = false;
         }
     }
 }
