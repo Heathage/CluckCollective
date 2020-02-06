166c6f8526a7192512861f251deda6209b9490a2
James Heath <jh230155@falmouth.ac.uk>
2020-02-06 18:36:29 +0000

    Cleaned up code on PushingObjectsV2 Left to be reviewed.

--- Assets/NextDayDelivery/Scripts/World Objects/PushingObjectsV2.cs
+++ Assets/NextDayDelivery/Scripts/World Objects/PushingObjectsV2.cs
@@ -16,6 +16,11 @@ public class PushingObjectsV2 : MonoBehaviour
         }
     }
 
+    void OnCollisionExit(Collision other)
+    {
+        noPush();
+    }
+
     private void push()
     {
         Debug.Log("Sucess");
@@ -27,34 +32,4 @@ public class PushingObjectsV2 : MonoBehaviour
         gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
     }
 }
-
-    //public LayerMask pushMask;
-
-    //void Update()
-    //{
-    //    if (Input.GetKey(KeyCode.Mouse0))
-    //    {
-    //        PushObject();
-    //    }
-    //    else
-    //    {
-    //    }
-    //    Debug.DrawRay(transform.position, transform.forward * 2f, Color.blue);
-    //}
-    //void PushObject()
-    //{
-    //    RaycastHit pushable;
-
-    //    if (Physics.Raycast(transform.position, transform.forward, out pushable, 2f, pushMask))
-    //    {
-    //        Debug.Log("Sucess");
-    //        pushable.collider.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-    //        pushable.collider.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
-    //    }
-    //    else
-    //    {
-    //        pushable.collider.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX;
-    //        pushable.collider.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ;
-    //    }
-    //}
 
