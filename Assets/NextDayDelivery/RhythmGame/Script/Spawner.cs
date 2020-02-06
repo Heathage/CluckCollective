67288e2f7700e9b9cb11aab8525a050aa925ceac
Isaac <isaaclouiswright@gmail.com>
2020-02-06 15:30:57 +0000

    Added the ability for the spawner script to: Spawn a level based on a given level number have each level be a different length from one another

--- Assets/NextDayDelivery/RhythmGame/Script/Spawner.cs
+++ Assets/NextDayDelivery/RhythmGame/Script/Spawner.cs
@@ -6,23 +6,28 @@ using TMPro;
 public class Spawner : MonoBehaviour
 {
     public GameObject note;
-    private string[,] lvOne  = new string[,]{ { "z", "v", "n", "d", "f", "i" }, {"s", "h", "r", "e", "k", "4"} }; //Array of strings that are output by the spawner
-    private int est = 1;
-    
+
+    private string[][] lvOne = new string[][] { new[] { "z", "v", "n", "d", "f", "i" },
+        new[] { "s", "h", "r", "e", "k", }, 
+        new[] { "s", "h", "r", "e", "k", "2" },
+        new[] { "s", "h", "r", "e", "k", "3" }, 
+        new[] { "s", "h", "r", "e", "k", "4" } }; //A multidimentional string array that's output by the spawner. The new[] before each array allows them to be of any length.
+
+
+    public int level; //Must be replaced with an int that actually represents the level number
+
     void Awake()
     {
-        StartCoroutine(Test());
+        StartCoroutine(LevelTiming()); //Allows the script to use WaitForSeconds
     }
 
-
-    // Update is called once per frame
-    IEnumerator Test()
+    IEnumerator LevelTiming() //Lanuches a time sensitive function
     {
-        for (int i = 0; i < lvOne.Length; i++) // for every item between item equal to 0 and item equal to the length of the lvOne string (in this case 3)
+        for (int i = 0; i < lvOne[(level)].Length; i++) // for every item between item equal to 0 and item equal to the length of the lvOne string (in this case 3)
         {
-            note.GetComponent<Keypress>().keyType = (lvOne[(est),i]); //Sets the keytype of the note to the current item in the array
+            note.GetComponent<Keypress>().keyType = (lvOne[(level)][i]); //Sets the keytype of the note to the current item in the array
             Instantiate(note, gameObject.transform); //instantiates each note
-            yield return new WaitForSeconds(1/3f); //Waits for an amount
+            yield return new WaitForSeconds(1 / 3f); //Waits for an amount
         }
     }
 }
