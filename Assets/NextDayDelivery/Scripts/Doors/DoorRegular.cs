9241d738b733c53fd3cd4d051e4aa1b9a012d4f7
Lyra <ts231024@falmouth.ac.uk>
2020-02-10 13:14:34 +0000

    Begun working on door stuff in a new branch to avoid fuckery

---
+++ Assets/NextDayDelivery/Scripts/Doors/DoorRegular.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorRegular : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter()
+    {
+        anim.SetBool("Open?", true);
+    }
+
+    void OnTriggerExit()
+    {
+        //anim.SetBool("Open?", false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
