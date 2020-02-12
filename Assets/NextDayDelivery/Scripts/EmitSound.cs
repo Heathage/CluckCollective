d088e56e376c208d933332b15347ff08ef17a745
Callum <callummgamedev@hotmail.com>
2020-02-12 14:20:07 +0000

    Now checks if the box has hit the ground

---
+++ Assets/NextDayDelivery/Scripts/EmitSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmitSound : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 velocity;
+    private void Awake()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        velocity = rb.velocity;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (velocity.y <= 1)
+        {
+            Debug.Log("Sound");
+        }
+    }
+}
