4607e9dc00595436c651f72d972d553b37af3bc3
Isaac <isaaclouiswright@gmail.com>
2020-02-14 11:58:53 +0000

    Removed old prefabs, added ability to fail minigame
    Added a fail hitbox, as well as created a script that gives the player 4 misses until they fail the challenge.

--- Assets/NextDayDelivery/RhythmGame/Script/Keypress.cs
+++ Assets/NextDayDelivery/RhythmGame/Script/Keypress.cs
@@ -7,21 +7,24 @@ public class Keypress : MonoBehaviour
 {
     public GameObject hit;
     public string keyType;
-
-    // Start is called before the first frame update
-
+    
     void Update()
     {
-        transform.Translate(-Vector3.right * Time.deltaTime / 2); //Moves each note right slowly
+        transform.Translate(-Vector3.right * Time.deltaTime / 2); //Moves each note left slowly
         //int test = GetComponent<StudioGlobalParameterTrigger>().OverrideValue;
         TextMeshPro textmeshPro = GetComponent<TextMeshPro>(); //references the textmeshpro script attatched to the note
         textmeshPro.SetText(keyType); //sets the text attatched to textmesh pro as the key type, which is assigned in the spawner script
     }
-    void OnTriggerStay2D(Collider2D hit) //If the notes collide with a 2D collider
+    void OnTriggerEnter2D(Collider2D hit) //If the notes collide with a 2D collider
     {
         if (Input.GetKey(keyType)) //and if the input key is equal to the keytype
         {
             Destroy(gameObject); //destroy the note object
+        }
+
+        if(hit.CompareTag("RhythmFail"))
+        {
+            Destroy(gameObject);
         }
     }
 }
