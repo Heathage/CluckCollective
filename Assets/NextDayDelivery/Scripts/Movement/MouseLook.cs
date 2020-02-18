0f2fb3ceb7f50dcf6ea14132252af4beeea32fb6
James Heath <jh230155@falmouth.ac.uk>
2020-02-18 14:45:30 +0000

    Added New Script ObjPickUp.cs Will be a new solution for picking up items. Added line of code to MouseLook.cs A new bool variable that will be accompanying new script solution.

--- Assets/NextDayDelivery/Scripts/Movement/MouseLook.cs
+++ Assets/NextDayDelivery/Scripts/Movement/MouseLook.cs
@@ -8,6 +8,9 @@ public class MouseLook : MonoBehaviour
     private float mouseSensitivity = 100f;
 
     [Space]
+    public bool canLook = true;
+
+    [Space]
     public Transform playerBody;
 
     float xRotation = 0f;
@@ -20,15 +23,18 @@ public class MouseLook : MonoBehaviour
 
     void Update()
     {
-        //Allows the player to look around with the mouse.
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        if (canLook == true)
+        {
+            //Allows the player to look around with the mouse.
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        xRotation -= mouseY;
-        //Ensures that the player can only look up and down to a maximum of 90 degrees. 
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            xRotation -= mouseY;
+            //Ensures that the player can only look up and down to a maximum of 90 degrees. 
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
     }
 }
