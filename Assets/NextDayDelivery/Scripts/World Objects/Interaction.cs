ed7842c211c2d239656d3d21959c534b302f11b3
James Heath <jh230155@falmouth.ac.uk>
2020-02-20 14:38:36 +0000

    Added Interaction.cs Removed ObjPickUp.cs Added updated version of ObjPickUp.cs ObjPickUp.cs works similar to ObjInteraction.cs Code is better laid out with calls to methods. Picking up objects is the same with same bugs.

---
+++ Assets/NextDayDelivery/Scripts/World Objects/Interaction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    [SerializeField]
+    private float playerReach = 0f;
+
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.forward * playerReach, Color.black);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if(Physics.Raycast(transform.position, transform.forward, out hit, playerReach))
+            {
+                hit.collider.GetComponent<ObjPickUp>()?.Interact();
+            }
+        }
+    }
+}
--- Assets/NextDayDelivery/Scripts/World Objects/ObjPickUp.cs
+++ Assets/NextDayDelivery/Scripts/World Objects/ObjPickUp.cs
@@ -4,16 +4,94 @@ using UnityEngine;
 
 public class ObjPickUp : MonoBehaviour
 {
-    public MouseLook freezeCam;
+    Vector3 objectPos;
 
+    //public MouseLook freezeCam;
     [SerializeField]
     private bool holding = false;
     [SerializeField]
     private bool inspecting = false;
 
-    public float holdDistance = 0f;
+    [SerializeField]
+    public GameObject item;
+    [SerializeField]
+    public GameObject tempParent;
 
-    
+    private float distance = 2f;
+    [SerializeField]
+    private float dropDistance = 0;
+    [SerializeField]
+    private float throwForce = 600;
+    public bool wasThrown = false;
+
+    //public float holdDistance = 0f;
+
+    public void Interact()
+    {
+        Debug.Log("Interacted");
+
+        if(holding == false)
+        {
+            holding = true;
+            item.GetComponent<Rigidbody>().useGravity = false;
+            Debug.Log("True");
+            //pick up item
+        }
+
+        else if(holding == true)
+        {
+            holding = false;
+            Debug.Log("False");
+            //dont pick up item
+        }
+    }
+    void Update()
+    {
+        distance = Vector3.Distance(item.transform.position, tempParent.transform.position);
+
+        if (distance >= dropDistance)
+        {
+            holding = false;
+        }
+
+        if (holding)
+        {
+            heldItem();
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                yeet();
+            }
+        }
+
+        else
+        {
+            dropped();
+        }
+    }
+
+    void heldItem()
+    {
+        item.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        item.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        item.transform.SetParent(tempParent.transform, true);
+    }
+
+    void yeet()
+    {
+        item.GetComponent<Rigidbody>().AddForce(tempParent.transform.forward * throwForce);
+        holding = false;
+        wasThrown = true;
+    }
+
+    void dropped()
+    {
+        objectPos = item.transform.position;
+        item.transform.SetParent(null);
+        item.GetComponent<Rigidbody>().useGravity = true;
+        item.transform.position = objectPos;
+    }
+
 
     //check to see if player can pick up item.
     //assign a button to player to pick up.
