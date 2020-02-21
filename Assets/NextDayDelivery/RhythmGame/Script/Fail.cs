6067287ab78a7b1cfeba1b14653fef584a777e2b
Isaac <isaaclouiswright@gmail.com>
2020-02-21 12:04:42 +0000

    Re-Added FMOD, fixed rhythm game collision, added working music and variables to rhythm game
    Fixed the FMOD files so they now work with the unity project, as well as re-implimenting the project. Also resized rhythm game hitboxes and made collision constant

--- Assets/NextDayDelivery/RhythmGame/Script/Fail.cs
+++ Assets/NextDayDelivery/RhythmGame/Script/Fail.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class Fail : MonoBehaviour
 {
+    FMOD.Studio.EventInstance Audiooo;
     private int failLevel = 4;
-    void OnTriggerEnter2D(Collider2D note) //If the notes collide with a 2D collider
+
+    private void Start()
+    {
+        Audiooo = FMODUnity.RuntimeManager.CreateInstance("event:/Audiooo");
+        Audiooo.start();
+        Audiooo.setParameterByName("Section", 5);
+    }
+    void OnTriggerEnter2D(Collider2D note) //If anything collides with the object
     {
         failLevel -= 1;
         Debug.Log(failLevel);
         if (failLevel == 0)
+            Audiooo.setParameterByName("Success Level", failLevel);
         {
             Debug.Log("Game Over!");
         }
--- Assets/NextDayDelivery/RhythmGame/Script/Keypress.cs
+++ Assets/NextDayDelivery/RhythmGame/Script/Keypress.cs
@@ -8,15 +8,14 @@ public class Keypress : MonoBehaviour
 {
     public GameObject hit;
     public string keyType;
-    
+
     void Update()
     {
         transform.Translate(-Vector3.right * Time.deltaTime / 2); //Moves each note left slowly
-        //int test = GetComponent<StudioGlobalParameterTrigger>().OverrideValue;
         TextMeshPro textmeshPro = GetComponent<TextMeshPro>(); //references the textmeshpro script attatched to the note
         textmeshPro.SetText(keyType); //sets the text attatched to textmesh pro as the key type, which is assigned in the spawner script
     }
-    void OnTriggerEnter2D(Collider2D hit) //If the notes collide with a 2D collider
+    void OnTriggerStay2D(Collider2D hit) //If the notes collide with a 2D collider
     {
         if (Input.GetKey(keyType)) //and if the input key is equal to the keytype
         {
