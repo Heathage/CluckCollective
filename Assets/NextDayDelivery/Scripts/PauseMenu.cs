45a0019850a1d6cd17926b8e375b36cbbe87ad5d
Benjamin Emdon <57227860+BenjaminEmdon@users.noreply.github.com>
2020-03-03 16:55:57 +0000

    just pushing half a menu cos of

---
+++ Assets/NextDayDelivery/Scripts/PauseMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+
+    }
+
+    void Pause()
+    {
+
+    }
+}
